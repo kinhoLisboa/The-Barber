4179a5f19791577c3c26648428b6eb7bf5652eb5
kinhoLisboa <danifleikson@hotmail.com>
2025-02-26 20:08:22 -0300

    Mesclando branch barber em client

--- src/main/java/com/theBarber/TheBarber/Client/model/Client.java
+++ src/main/java/com/theBarber/TheBarber/Client/model/Client.java
@@ -1,16 +1,16 @@
 package com.theBarber.TheBarber.Client.model;
 
+import com.theBarber.TheBarber.Barber.model.Appointment;
 import com.theBarber.TheBarber.Client.DTO.CLientRequest;
 import com.theBarber.TheBarber.Client.DTO.UpdateClient;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 @Entity
 @Getter
@@ -21,10 +21,11 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-
     private String name;
     private String email;
     private String phone;
+    @OneToMany(mappedBy = "barber")
+    private List<Appointment> appointments = new ArrayList<>();
 
     public Client(CLientRequest request) {
         this.id = request.id();
